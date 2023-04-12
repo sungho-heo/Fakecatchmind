ea8c091688695cf61552940e87be0378732e25df
Sungho <hurgj123kr@gmail.com>
2023-04-12 20:30:19 +0900

    server running

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+
+const handelListen = (req, res) => {
+    console.log(`✅ Server running: http://localhost:${PORT}/`);
+}
+
+app.listen(PORT, handelListen);
