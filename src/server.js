c39d1ebd6ce6712e919074bf319328a07d610d7b
Sungho <hurgj123kr@gmail.com>
2023-04-13 19:17:32 +0900

    socket io on connetction complet

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import socketIO from "socket.io";
+import { Server } from "socket.io"
 // import apiRouter from "./router/apiRouter";
 
 const app = express();
@@ -8,16 +8,22 @@ const loggerMorgan = morgan("dev");
 const PORT = 4000;
 
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(loggerMorgan);
+app.use(express.static(process.cwd() +  "/src/static"));
+app.get("/", (req, res) => { res.render("home") });
+
 const handelListen = (req, res) => {
     console.log(`✅ Server running: http://localhost:${PORT}/`);
 }
 
-app.set("views", process.cwd() + "/src/views");
-app.set("view engine", "pug");
-app.use(express.static(process.cwd() +  "/src/static"));
-app.get("/", (req, res) => { res.render("home") });
-app.use(loggerMorgan);
 // app.use("/api", apiRouter);
 const server = app.listen(PORT, handelListen);
 
-const io = socketIO(server);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log(socket);
+});
+
