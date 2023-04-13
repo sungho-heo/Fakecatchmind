b00d4c123745558c4fc24c8600e4aa57fc34373c
Sungho <hurgj123kr@gmail.com>
2023-04-13 15:00:55 +0900

    socket io use

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import socketIO from "socket.io";
 // import apiRouter from "./router/apiRouter";
 
 const app = express();
@@ -11,11 +12,12 @@ const handelListen = (req, res) => {
     console.log(`✅ Server running: http://localhost:${PORT}/`);
 }
 
-console.log(process.cwd() + "/src/views");
 app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
 app.use(express.static(process.cwd() +  "/src/static"));
 app.get("/", (req, res) => { res.render("home") });
 app.use(loggerMorgan);
 // app.use("/api", apiRouter);
-app.listen(PORT, handelListen);
+const server = app.listen(PORT, handelListen);
+
+const io = socketIO(server);
