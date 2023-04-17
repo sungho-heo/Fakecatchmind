35f5017516af537c49d788206ee5c09a129307b8
Sungho <hurgj123kr@gmail.com>
2023-04-17 14:31:52 +0900

    mouse moveto and line sketch add

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -4,19 +4,13 @@ const context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-context.rect(50, 50, 100, 100);
-context.rect(100, 100, 100, 100);
-context.rect(150, 150, 100, 100);
-context.rect(200, 200, 100, 100);
-context.rect(250, 250, 100, 100);
-context.fill();
 
-context.beginPath();
-context.rect(300, 300, 100, 100);
-context.rect(350, 350, 100, 100);
-context.rect(400, 400, 100, 100);
-context.rect(450, 450, 100, 100);
-context.fillStyle = "red";
+context.moveTo(50, 50);
+context.lineTo(150, 50);
+context.lineTo(150, 150);
+context.lineTo(50, 150);
+context.lineTo(50, 50);
+// context.stroke();
 context.fill();
 
 context.beginPath();
