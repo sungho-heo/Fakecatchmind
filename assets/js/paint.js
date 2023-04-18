985c833ce23218ee04eaf7512209d7e4e70e77fe
Sungho <hurgj123kr@gmail.com>
2023-04-18 13:48:08 +0900

    paint line width change

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,11 +1,13 @@
 const canvas = document.querySelector("canvas");
 const eraser = document.getElementById("canvas__delete");
+const line__width = document.getElementById("line__width");
 const context = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-
 let painting = false;
+context.lineWidth = line__width.value;
+
 
 
 
@@ -15,7 +17,6 @@ const onMove = (event) => {
     if (painting) {
         context.lineTo(x, y);
         context.stroke();
-        console.log(x, y);
     };
     context.moveTo(x, y);
 };
@@ -26,18 +27,23 @@ const startPainting = () => {
 
 const endPainting = () => {
     painting = false;
+    context.beginPath();
 }
 
 const handleEraser = () => {
     context.reset();
 }
 
+const handleLineWidth = (event) => {
+    context.lineWidth = event.target.value;
+};
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", endPainting);
 canvas.addEventListener("mouseleave", endPainting);
 eraser.addEventListener("click", handleEraser);
-
+line__width.addEventListener("change", handleLineWidth);
 
 
 
