78b4507bf681e84f4a493792a76bfd77285e441b
Sungho <hurgj123kr@gmail.com>
2023-04-18 19:29:57 +0900

    shortcut change color create

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -3,6 +3,7 @@ const lineRange = document.getElementById("line__width");
 const color = document.getElementById("color");
 const eraser = document.getElementById("canvas__delete");
 const context = canvas.getContext("2d");
+const gameColor = Array.from(document.getElementsByClassName("gameColor"));
 
 canvas.width = 800;
 canvas.height = 800;
@@ -39,11 +40,22 @@ const handleLineRange = (event) => {
     context.lineWidth = event.target.value;
 }
 
+
+const colorValue = (color) => {
+    context.fillStyle = color;
+    context.strokeStyle = color;
+}
+
 const handleColor = (event) => {
     const value = event.target.value;
-    context.fillStyle = value;
-    context.strokeStyle = value;
+    colorValue(value);
 }
+
+const handleColorChange = (event) => {
+    const colorNumber = event.target.dataset.color;
+    colorValue(colorNumber);
+    color.value = colorNumber;
+};
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
@@ -52,6 +64,7 @@ canvas.addEventListener("mouseleave", endPainting);
 eraser.addEventListener("click", handleEraser);
 color.addEventListener("change", handleColor);
 lineRange.addEventListener("change", handleLineRange);
+gameColor.forEach((color) => color.addEventListener("click", handleColorChange));
 
 
 
