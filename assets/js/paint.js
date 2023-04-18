93826aada6fea2c33664ab3a79413699608d0893
Sungho <hurgj123kr@gmail.com>
2023-04-18 09:51:29 +0900

    mousemove and click stoke line

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -4,26 +4,34 @@ const context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
+const colors = [
+    "#FF6D60",
+    "#98D8AA",
+    "#C0DBEA",
+    "#BA90C6",
+    "#DDFFBB",
+    "#FEDEFF",
+];
 
-// context.rect(200, 200, 50, 200);
-// context.rect(500, 200, 50, 200);
-// context.rect(350, 300, 50, 100);
-// context.moveTo(200, 200);
-// context.lineTo(380, 100);
-// context.lineTo(550, 200);
-// context.fill();
+const moveChange = (x, y) => {
+    context.beginPath();
+    context.moveTo(x, y);
+};
 
-context.fillRect(250, 200, 20, 200);
-context.fillRect(370, 200, 20, 200);
-context.fillRect(280, 200, 80, 260);
+const onClick = (event) => {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    moveChange(0, 0);
+    context.lineTo(x, y);
+    context.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+    context.stroke();
+    moveChange(400, 400);
+    context.lineTo(x, y);
+    context.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+    context.stroke();    
+}
 
-context.arc(320, 150, 50, 0, 2 * Math.PI);
-context.fill();
 
-context.beginPath();
-context.arc(300, 140, 10, Math.PI, 0);
-context.arc(340, 140, 10, Math.PI, 0);
-context.fillStyle = "blue";
-context.fill();
+canvas.addEventListener("mousemove", onClick);
 
 
